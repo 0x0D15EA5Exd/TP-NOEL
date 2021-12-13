9a19ee26a27808c7bf6d050ac9cc459291cff9bd
0xDEADCODE <permabanxviii@protonmail.com>
2021-12-13 11:07:15 +0100

    Initial Commit

---
+++ TP-NOEL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TP-NOEL
+//
+//  Created by Victor on 13/12/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
